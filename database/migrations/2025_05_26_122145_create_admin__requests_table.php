c486d7f10cb9d771256b310423e0dd878c10fa51
anggitasalsabilla <anggitasalsabilla20@gmail.com>
2025-05-26 19:25:07 +0700

    Tambah tabel Admin_Request

---
+++ database/migrations/2025_05_26_122145_create_admin__requests_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('admin__requests', function (Blueprint $table) {
+            $table->id()->primary()->autoIncrement();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('admin__requests');
+    }
+};
