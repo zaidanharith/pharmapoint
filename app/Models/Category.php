c40d3104a34736a285a35b9bf69d660d8c858f22
Zaidan Harith <myharithzaidan@gmail.com>
2025-05-28 01:04:43 +0700

    Membuat Database Factory dan Membuat Relationship Category-Medicine

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,9 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
     /** @use HasFactory<\Database\Factories\CategoryFactory> */
     use HasFactory;
+
+    protected $fillable = ['name'];
+
+    public function category() : HasMany {
+        return $this->hasMany(Medicines::class,'category_id');
+    }
 }
--- app/Models/Medicines.php
+++ app/Models/Medicines.php
@@ -2,11 +2,25 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Medicines extends Model
 {
     /** @use HasFactory<\Database\Factories\MedicinesFactory> */
     use HasFactory;
+
+    protected $fillable = ['name','slug','price','stock', 'category_id'];
+
+    public function category() : BelongsTo {
+        return $this->belongsTo(Category::class);
+    }
+
+    // public function setSlugAttribute($value)
+    // {
+    //     $this->attributes['slug'] = Str::slug($this->name);
+    // }
 }
