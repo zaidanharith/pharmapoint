f43f1d0266b31b498af0ac77a80993518e17cf56
Zaidan Harith <myharithzaidan@gmail.com>
2025-06-04 22:20:14 +0700

    tambah middleware

---
+++ app/Http/Kernel.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http;
+
+use App\Http\Middleware\AdminMiddleware;
+use Illuminate\Auth\Middleware\Authenticate;
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+use Illuminate\Auth\Middleware\RedirectIfAuthenticated;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
+
+class Kernel extends HttpKernel
+{
+    /**
+     * The application's global HTTP middleware stack.
+     *
+     * @var array<int, class-string|string>
+     */
+    protected $middleware = [
+        \Illuminate\Http\Middleware\HandleCors::class,
+        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+    ];
+
+    /**
+     * The application's route middleware groups.
+     *
+     * @var array<string, array<int, class-string|string>>
+     */
+    protected $middlewareGroups = [
+        'web' => [
+            \Illuminate\Cookie\Middleware\EncryptCookies::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+
+        'api' => [
+            EnsureFrontendRequestsAreStateful::class,
+            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+    ];
+
+    /**
+     * The application's middleware aliases.
+     *
+     * @var array<string, class-string|string>
+     */
+    protected $middlewareAliases = [
+        'auth' => Authenticate::class,
+        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'guest' => RedirectIfAuthenticated::class,
+        'admin' => AdminMiddleware::class,
+    ];
+}
---
+++ resources/views/tambah-katalog.blade.php
@@ -0,0 +1,10 @@
+<x-layout>
+     <x-slot:title>{{ $title }}</x-slot:title>
+
+     <section id="catalogue" class="py-30 text-black">
+          <div class="container mx-auto px-4">
+            <h1>Tambah Katalog</h1>
+          </div>
+        </section>
+     
+</x-layout>
