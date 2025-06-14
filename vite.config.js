d6bdc5daf364c4521f6b0b0d31f3d95157763645
Zaidan Harith <myharithzaidan@gmail.com>
2025-06-14 13:44:32 +0700

    add manifest:true

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
         }),
     ],
     build: {
-        manifest: false,
+        manifest: true,
         rollupOptions: {
             input: ["resources/css/app.css", "resources/js/app.js"],
         },
