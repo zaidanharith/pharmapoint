b8a3da713bcca4488b8f2127684569c24ccd97d4
Zaidan Harith <myharithzaidan@gmail.com>
2025-06-14 22:31:03 +0700

    Fixing Horizontal Scroll

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,7 +2,7 @@
 <html lang="id" class="scroll-smooth">
   <head>
     <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
     <title>{{ $title }} - PharmaPoint+</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" />
@@ -11,4 +11,4 @@
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
   </head>
-  <body class="font-jakarta selection:text-blue-dark selection:bg-peach">
+  <body class="font-jakarta selection:text-blue-dark selection:bg-peach overflow-x-hidden">
